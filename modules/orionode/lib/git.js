0e891eb4d29fe9a03ab1801e734d4ab4e33b7532
Marcin Piekarniak <mp306415@students.mimuw.edu.pl>
2013-02-18 22:31:16 +0100

    Added stub restful api handling

--- modules/orionode/lib/git.js
+++ modules/orionode/lib/git.js
@@ -17,7 +17,6 @@ var url = require('url');
 var api = require('./api'), write = api.write, writeError = api.writeError;
 var fileUtil = require('./fileUtil'), ETag = fileUtil.ETag;
 var resource = require('./resource');
-
 var USER_WRITE_FLAG = parseInt('0200', 8);
 var USER_EXECUTE_FLAG = parseInt('0100', 8);
 
@@ -27,6 +26,24 @@ var USER_EXECUTE_FLAG = parseInt('0100', 8);
  * Module begins here
  *
  */
+ 
+function doJob(handler, rest, req, res) {
+	var methodName = rest[0] === '/' ? rest.split('/')[1] : rest.split('/')[0];
+	var method = handler[methodName];
+	var queryData = "";
+	req.on('data', function(data) {
+		queryData += data;
+		if(queryData.length > 1e6) {
+			queryData = "";
+			res.writeHead(413, {'Content-Type': 'text/plain'});
+			req.connection.destroy();
+		}
+	});
+	req.on('end', function() {
+		write(200, res, null, method(rest, queryData));
+	});    
+}
+
 module.exports = function(options) {
 	
 	var fileRoot = options.root;
@@ -37,22 +54,230 @@ module.exports = function(options) {
 	return connect()
 	.use(connect.json())
 	.use(resource(fileRoot, {
-		//jak sie to wywali, to przestaje dzialac :) nie mam pojecia
-		//do czego to jest potrzebne, ale w przegladarce widac efekt -
-		//na kazdej podstronie gita wyswietla sie blad :)
 		GET: function(req, res, next, rest) {
-			writeError(404, res, 'File not found: ');
+			var handler = getHandlers;
+			doJob(handler, rest, req, res);
 		},
 
 		POST: function(req, res, next, rest) {
-			console.log("obsluga requesta\n");
-			req.on('data', function(data) {
-				console.log(data.toString('utf8', 0, data.length));
-				writeError(500, res, "blad");
-			});
-				
-
+			var handler = posttHandlers;
+			doJob(handler, rest);
+			
+		},
+		DELETE: function(req, res, next, rest) {
+			var handler = deleteHandlers;
+			doJob(handler, rest);
+		},
+	
+		PUT: function(req, res, next, res) {
+			var handler = putHandlers;
+			doJob(handler, rest);
 		},
 
 	}));
 };
+
+getHandlers = {
+	branch : getBranch,
+	clone : getClone,
+	commit : getCommit,
+	config : getConfig,
+	diff : getDiff,
+	index : getIndex,
+	remote : getRemote,
+	status : getStatus,
+	tag : getTag
+};
+putHandlers = {
+	branch : putBranch,
+	clone : putClone,
+	commit : putCommit,
+	config : putConfig,
+	diff : putDiff,
+	index : putIndex,
+	remote : putRemote,
+	status : putStatus,
+	tag : putTag
+};
+postHandlers = {
+	branch : postBranch,
+	clone : postClone,
+	commit : postCommit,
+	config : postConfig,
+	diff : postDiff,
+	index : postIndex,
+	remote : postRemote,
+	status : postStatus,
+	tag : postTag
+};
+deleteHandlers = {
+	branch : deleteBranch,
+	clone : deleteClone,
+	commit : deleteCommit,
+	config : deleteConfig,
+	diff : deleteDiff,
+	index : deleteIndex,
+	remote : deleteRemote,
+	status : deleteStatus,
+	tag : deleteTag
+};
+
+function getBranch(rest, queryData) {
+	
+}
+
+function getClone(rest, queryData) {
+	return { 
+  "Children": [ 
+    { 
+      "BranchLocation": "/gitapi/branch/file/ilovegame/Hello%20World/", 
+      "CommitLocation": "/gitapi/commit/file/ilovegame/Hello%20World/", 
+      "ConfigLocation": "/gitapi/config/clone/file/ilovegame/Hello%20World/", 
+      "ContentLocation": "/file/ilovegame/Hello%20World/", 
+      "DiffLocation": "/gitapi/diff/Default/file/ilovegame/Hello%20World/", 
+      "HeadLocation": "/gitapi/commit/HEAD/file/ilovegame/Hello%20World/", 
+      "IndexLocation": "/gitapi/index/file/ilovegame/Hello%20World/", 
+      "Location": "/gitapi/clone/file/ilovegame/Hello%20World/", 
+      "Name": "Hello World", 
+      "RemoteLocation": "/gitapi/remote/file/ilovegame/Hello%20World/", 
+      "StatusLocation": "/gitapi/status/file/ilovegame/Hello%20World/", 
+      "TagLocation": "/gitapi/tag/file/ilovegame/Hello%20World/", 
+      "Type": "Clone" 
+    }
+  ], 
+  "Type": "Clone" 
+};
+}
+
+function getCommit(rest, queryData) {
+	
+}
+
+function getConfig(rest, queryData) {
+	
+}
+function getDiff(rest, queryData) {
+	
+}
+
+function getIndex(rest, queryData) {
+	
+}
+
+function getRemote(rest, queryData) {
+	
+}
+
+function getStatus(rest, queryData) {
+	
+}
+
+function getTag(rest, queryData) {
+	
+}
+
+
+function postBranch(rest, queryData) {
+	
+}
+
+function postClone(rest, queryData) {
+	
+}
+
+function postCommit(rest, queryData) {
+	
+}
+
+function postConfig(rest, queryData) {
+	
+}
+function postDiff(rest, queryData) {
+	
+}
+
+function postIndex(rest, queryData) {
+	
+}
+
+function postRemote(rest, queryData) {
+	
+}
+
+function postStatus(rest, queryData) {
+	
+}
+
+function postTag(rest, queryData) {
+	
+}
+
+function putBranch(rest, queryData) {
+	
+}
+
+function putClone(rest, queryData) {
+	
+}
+
+function putCommit(rest, queryData) {
+	
+}
+
+function putConfig(rest, queryData) {
+	
+}
+function putDiff(rest, queryData) {
+	
+}
+
+function putIndex(rest, queryData) {
+	
+}
+
+function putRemote(rest, queryData) {
+	
+}
+
+function putStatus(rest, queryData) {
+	
+}
+
+function putTag(rest, queryData) {
+	
+}
+
+function deleteBranch(rest, queryData) {
+	
+}
+
+function deleteClone(rest, queryData) {
+	
+}
+
+function deleteCommit(rest, queryData) {
+	
+}
+
+function deleteConfig(rest, queryData) {
+	
+}
+function deleteDiff(rest, queryData) {
+	
+}
+
+function deleteIndex(rest, queryData) {
+	
+}
+
+function deleteRemote(rest, queryData) {
+	
+}
+
+function deleteStatus(rest, queryData) {
+	
+}
+
+function deleteTag(rest, queryData) {
+	
+}
