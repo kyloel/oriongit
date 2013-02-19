1a608fbe40a02ec6ddaa80516f13f1d7dc50b334
Maciej Sabat <msabat@aol.pl>
2013-02-19 18:34:28 +0100

    Init and list of repositories work

--- modules/orionode/lib/git.js
+++ modules/orionode/lib/git.js
@@ -12,6 +12,7 @@
 var compat = require('./compat');
 var connect = require('connect');
 var fs = require('fs');
+var git = require('gitnode');
 var path = require('path');
 var url = require('url');
 var api = require('./api'), write = api.write, writeError = api.writeError;
@@ -27,7 +28,8 @@ var USER_EXECUTE_FLAG = parseInt('0100', 8);
  *
  */
  
-function doJob(handler, rest, req, res) {
+function doJob(handler, rest, req, res, workspaceDir) {
+	//TODO handle invalid requests
 	var methodName = rest[0] === '/' ? rest.split('/')[1] : rest.split('/')[0];
 	var method = handler[methodName];
 	var queryData = "";
@@ -35,14 +37,20 @@ function doJob(handler, rest, req, res) {
 		queryData += data;
 	});
 	req.on('end', function() {
-		write(200, res, null, method(rest, queryData));
-	});    
+		var json = null;
+		if (queryData.length > 0)
+		{
+			json = JSON.parse(queryData);
+		}
+		method(res, rest, json, workspaceDir);
+	});
 }
 
 module.exports = function(options) {
-	
+	console.log("OPT: " + options);
+	console.log("OPT: " + url);
 	var fileRoot = options.root;
-
+	var workspaceDir = options.workspaceDir;
 	/*
 	 * Handler begins here
 	 */
@@ -51,22 +59,22 @@ module.exports = function(options) {
 	.use(resource(fileRoot, {
 		GET: function(req, res, next, rest) {
 			var handler = getHandlers;
-			doJob(handler, rest, req, res);
+			doJob(handler, rest, req, res, workspaceDir);
 		},
 
 		POST: function(req, res, next, rest) {
 			var handler = postHandlers;
-			doJob(handler, rest, req, res);
+			doJob(handler, rest, req, res, workspaceDir);
 			
 		},
 		DELETE: function(req, res, next, rest) {
 			var handler = deleteHandlers;
-			doJob(handler, rest, req, res);
+			doJob(handler, rest, req, res, workspaceDir);
 		},
 	
 		PUT: function(req, res, next, res) {
 			var handler = putHandlers;
-			doJob(handler, rest, req, res);
+			doJob(handler, rest, req, res, workspaceDir);
 		},
 
 	}));
@@ -117,163 +125,229 @@ deleteHandlers = {
 	tag : deleteTag
 };
 
-function getBranch(rest, queryData) {
+function getBranch(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function getClone(rest, queryData) {
-	return { 
-  "Children": [ 
-    { 
-      "BranchLocation": "/gitapi/branch/file/ilovegame/Hello%20World/", 
-      "CommitLocation": "/gitapi/commit/file/ilovegame/Hello%20World/", 
-      "ConfigLocation": "/gitapi/config/clone/file/ilovegame/Hello%20World/", 
-      "ContentLocation": "/file/ilovegame/Hello%20World/", 
-      "DiffLocation": "/gitapi/diff/Default/file/ilovegame/Hello%20World/", 
-      "HeadLocation": "/gitapi/commit/HEAD/file/ilovegame/Hello%20World/", 
-      "IndexLocation": "/gitapi/index/file/ilovegame/Hello%20World/", 
-      "Location": "/gitapi/clone/file/ilovegame/Hello%20World/", 
-      "Name": "Hello World", 
-      "RemoteLocation": "/gitapi/remote/file/ilovegame/Hello%20World/", 
-      "StatusLocation": "/gitapi/status/file/ilovegame/Hello%20World/", 
-      "TagLocation": "/gitapi/tag/file/ilovegame/Hello%20World/", 
-      "Type": "Clone" 
-    }
-  ], 
-  "Type": "Clone" 
-};
+function getClone(res, rest, dataJson, workspaceDir) {
+	//GET /gitapi/clone/workspace/P/ on wiki, but in fact - it's different
+	//rest == clone/workspace/orionode
+	var repositories = [];
+
+	function sendResponse()
+	{
+		var entries = [ ];
+		repositories.forEach(function (repository) {
+			//TODO
+			var entry = {
+				"BranchLocation": "/gitapi/branch/file/ilovegame/Hello%20World/", 
+				"CommitLocation": "/gitapi/commit/file/ilovegame/Hello%20World/", 
+				"ConfigLocation": "/gitapi/config/clone/file/ilovegame/Hello%20World/", 
+				"ContentLocation": "/file/ilovegame/Hello%20World/", 
+				"DiffLocation": "/gitapi/diff/Default/file/ilovegame/Hello%20World/", 
+				"HeadLocation": "/gitapi/commit/HEAD/file/ilovegame/Hello%20World/", 
+				"IndexLocation": "/gitapi/index/file/ilovegame/Hello%20World/", 
+				"Location": "/gitapi/clone/file/ilovegame/Hello%20World/", 
+				"Name": repository,
+				"RemoteLocation": "/gitapi/remote/file/ilovegame/Hello%20World/", 
+				"StatusLocation": "/gitapi/status/file/ilovegame/Hello%20World/", 
+				"TagLocation": "/gitapi/tag/file/ilovegame/Hello%20World/", 
+				"Type": "Clone" 
+			}
+			entries.push(entry);
+		});
+
+		var json = JSON.stringify( { "Children" : entries, "Type": "Clone" } );
+		write(200, res, null, json);
+	}
+
+	fs.readdir(workspaceDir, function (err, list)
+	{
+		if (err)
+		{
+			writeError(500, res, 'Error occured ');
+		}
+		
+		var filesCount = list.length;
+		var filesVerified = 0;
+		list.forEach(function (file) {
+			var path = workspaceDir + "/" + file;
+			fs.stat(path, function (err, stat) {
+				if (err)
+				{
+					writeError(500, res, 'Error occured ');
+				}
+				if (stat.isDirectory())
+				{
+					var gitDir = path+'/.git';
+					fs.exists(gitDir, function (exists) {
+						if (exists)
+						{
+							repositories.push(file);	
+						}
+						filesVerified++;
+						if (filesVerified === filesCount)
+						{
+							sendResponse();
+						}
+					});
+				}
+				else 
+				{
+					filesVerified++;
+					if (filesVerified === filesCount)
+					{
+						sendResponse();
+					}
+				}
+			});
+		});
+		
+		
+		
+	});
 }
 
-function getCommit(rest, queryData) {
-	
-}
-
-function getConfig(rest, queryData) {
-	
-}
-function getDiff(rest, queryData) {
-	
-}
-
-function getIndex(rest, queryData) {
-	
-}
-
-function getRemote(rest, queryData) {
-	
-}
-
-function getStatus(rest, queryData) {
-	
-}
-
-function getTag(rest, queryData) {
-	
-}
-
-
-function postBranch(rest, queryData) {
-	
-}
-
-function postClone(rest, queryData) {
-	console.log(queryData);
-	json = JSON.parse(queryData);
-}
-
-function postCommit(rest, queryData) {
-	
-}
-
-function postConfig(rest, queryData) {
-	
-}
-function postDiff(rest, queryData) {
-	
-}
-
-function postIndex(rest, queryData) {
-	
-}
-
-function postRemote(rest, queryData) {
-	
-}
-
-function postStatus(rest, queryData) {
-	
-}
-
-function postTag(rest, queryData) {
+function getCommit(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function putBranch(rest, queryData) {
+function getConfig(res, rest, dataJson, workspaceDir) {
+	
+}
+function getDiff(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function putClone(rest, queryData) {
+function getIndex(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function putCommit(rest, queryData) {
+function getRemote(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function putConfig(rest, queryData) {
-	
-}
-function putDiff(rest, queryData) {
+function getStatus(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function putIndex(rest, queryData) {
+function getTag(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function putRemote(rest, queryData) {
+
+function postBranch(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function putStatus(rest, queryData) {
+function postClone(res, rest, dataJson, workspaceDir) {
+	//TODO req: {"Name":"repo","Location":"/workspace/orionode"} - workspace or .workspace ?
+	var dir = workspaceDir + '/' + dataJson['Name'];
+	fs.mkdir(dir, function (err) {
+		if (err)
+		{
+			writeError(500, res, "Error occured");
+		}
+		git.git(dir);
+		//TODO location with url
+		var resJson = JSON.stringify({ "Location" :  rest + dataJson['Name'] });
+		write(201, res, null, resJson)
+	});
+}
+
+function postCommit(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function putTag(rest, queryData) {
+function postConfig(res, rest, dataJson, workspaceDir) {
+	
+}
+function postDiff(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function deleteBranch(rest, queryData) {
+function postIndex(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function deleteClone(rest, queryData) {
+function postRemote(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function deleteCommit(rest, queryData) {
+function postStatus(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function deleteConfig(rest, queryData) {
-	
-}
-function deleteDiff(rest, queryData) {
+function postTag(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function deleteIndex(rest, queryData) {
+function putBranch(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function deleteRemote(rest, queryData) {
+function putClone(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function deleteStatus(rest, queryData) {
+function putCommit(res, rest, dataJson, workspaceDir) {
 	
 }
 
-function deleteTag(rest, queryData) {
+function putConfig(res, rest, dataJson, workspaceDir) {
+	
+}
+function putDiff(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function putIndex(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function putRemote(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function putStatus(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function putTag(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function deleteBranch(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function deleteClone(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function deleteCommit(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function deleteConfig(res, rest, dataJson, workspaceDir) {
+	
+}
+function deleteDiff(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function deleteIndex(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function deleteRemote(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function deleteStatus(res, rest, dataJson, workspaceDir) {
+	
+}
+
+function deleteTag(res, rest, dataJson, workspaceDir) {
 	
 }
